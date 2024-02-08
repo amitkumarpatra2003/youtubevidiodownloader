a4d1b69f3a95749cb2f6edfe70eebb9a30fde95e
amitkumarpatra2003 <techwithamit8984@gmail.com>
2024-02-08 22:42:23 +0530

    fixed

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,14 +1,28 @@
+# This is a Youtube Vidio Downloader using Terminal
 from pytube import YouTube
 
 def Download(link):
     try:
         youtubeObject = YouTube(link)
-        video = youtubeObject.streams.get_highest_resolution()
-        video.download()
-        print("Download Completed Successfully")
+        streams = youtubeObject.streams.filter(progressive=True)
+        
+        print("Available video qualities:")
+        for i, stream in enumerate(streams):
+            print(f"{i+1}. {stream.resolution}")
+        
+        choice = int(input("Enter the number corresponding to the video quality you want to download: "))
+        selected_stream = streams[choice - 1]
+        
+        print("Downloading...")
+        selected_stream.download()
+        print("Download completed successfully.")
+        
     except Exception as e:
         print("An error occurred while downloading:", str(e))
 
 if __name__ == "__main__":
     link = input("Enter the YouTube video URL: ")
     Download(link)
+
+
+
